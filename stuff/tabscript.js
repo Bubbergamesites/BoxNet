beb815ac81adb2072c27d28ad3834470a0f1c648
Bubber <bubbertheclayblobofficial@gmail.com>
2026-05-07 06:10:14 -0400

    Add 'mouseenter' event to restore/reset logic

--- stuff/tabscript.js
+++ stuff/tabscript.js
@@ -79,7 +79,7 @@
     }
 
     // Only restore on actual input, not just "moving" back to the tab
-    ['mousedown', 'keydown', 'scroll'].forEach(evt => {
+    ['mousedown', 'mouseenter', 'keydown', 'scroll'].forEach(evt => {
         document.addEventListener(evt, () => {
             if (isHidden) restorePage();
             else resetTimer();
